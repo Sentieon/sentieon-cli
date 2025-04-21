8614002984e9dbaa69abcf9db541694a151e6b48
Don Freed <donfreed12@gmail.com>
2025-04-21 15:08:26 -0700

    Update the argument handling helper

--- sentieon_cli/argh_parser.py
+++ sentieon_cli/argh_parser.py
@@ -1,5 +1,3 @@
-import sys
-
 import argh
 
 
@@ -9,34 +7,12 @@ class CustomArghParser(argh.ArghParser):
     """
 
     def _parse_optional(self, arg_string):
-        # if it's an empty string, it was meant to be a positional
-        if not arg_string:
-            return None
-
-        # if it doesn't start with a prefix, it was meant to be positional
-        if not arg_string[0] in self.prefix_chars:
-            return None
-
-        # if the option string is present in the parser, return the action
-        if arg_string in self._option_string_actions:
-            action = self._option_string_actions[arg_string]
-            if sys.version_info.minor == 12:
-                return [(action, arg_string, None, None)]
-            elif sys.version_info.minor == 11:
-                return action, arg_string, None, None
-            elif sys.version_info.minor >= 8:
-                return action, arg_string, None
-
-        # if a long argument, convert hypens to underscores and match
-        if len(arg_string) > 2 and arg_string[1] in self.prefix_chars:
-            arg_string_normalized = "--" + arg_string[2:].replace("-", "_")
-            if arg_string_normalized in self._option_string_actions:
-                action = self._option_string_actions[arg_string_normalized]
-                if sys.version_info.minor == 12:
-                    return [(action, arg_string, None, None)]
-                elif sys.version_info.minor == 11:
-                    return action, arg_string, None, None
-                elif sys.version_info.minor >= 8:
-                    return action, arg_string, None
-
+        # normalize the string - replace hypens with underscores
+        if (
+            arg_string
+            and len(arg_string) > 2
+            and arg_string[0] in self.prefix_chars
+            and arg_string[1] in self.prefix_chars
+        ):
+            arg_string = "--" + arg_string[2:].replace("-", "_")
         return super()._parse_optional(arg_string)
