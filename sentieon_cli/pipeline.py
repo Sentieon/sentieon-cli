9026cc021f1325a26e5731ef0e48cf8c6a808078
Don Freed <donfreed12@gmail.com>
2025-05-13 19:59:23 -0700

    Add the pipeline.py file

---
+++ sentieon_cli/pipeline.py
@@ -0,0 +1,116 @@
+"""
+A pipeline class
+"""
+
+from abc import ABC, abstractmethod
+import argparse
+import multiprocessing as mp
+import pathlib
+import shutil
+from typing import Any, Dict, List
+
+from .dag import DAG
+from .executor import BaseExecutor, DryRunExecutor, LocalExecutor
+from .logging import get_logger
+from .scheduler import ThreadScheduler
+from .util import __version__, tmp
+
+
+class BasePipeline(ABC):
+    """A pipeline base class"""
+
+    params: Dict[str, Dict[str, Any]] = {}
+    positionals: Dict[str, Dict[str, Any]] = {}
+
+    @classmethod
+    def add_arguments(cls, parser: argparse.ArgumentParser):
+        for k, kwargs in cls.params.items():
+            flags = ["--" + k]
+            if "default" in kwargs and "type" not in kwargs:
+                kwargs["type"] = type(kwargs["default"])
+            if "flags" in kwargs:
+                flags = kwargs["flags"]
+                del kwargs["flags"]
+            parser.add_argument(*flags, **kwargs)
+
+        for k, kwargs in cls.positionals.items():
+            parser.add_argument(k, **kwargs)
+
+    def handle_arguments(self, args: argparse.Namespace):
+        """Update self using the argparse object"""
+        for k in self.params.keys():
+            assert k in self.__dict__
+            if k in args.__dict__:
+                setattr(self, k, getattr(args, k))
+        for k in self.positionals.keys():
+            assert k in self.__dict__
+            if k in args.__dict__:
+                setattr(self, k, getattr(args, k))
+
+    def setup_logging(self, args: argparse.Namespace) -> None:
+        self.logger = get_logger(__name__)
+        assert self.logger.parent
+        self.logger.parent.setLevel(args.loglevel)
+        self.logger.info("Starting sentieon-cli version: %s", __version__)
+
+    def __init__(self) -> None:
+        self.cores = mp.cpu_count()
+        self.numa_nodes: List[str] = []
+        self.dry_run = False
+
+    def main(self, args: argparse.Namespace) -> None:
+        """Run the DNAscope pipeline"""
+        self.handle_arguments(args)
+        self.setup_logging(args)
+        self.validate()
+        self.configure()
+
+        tmp_dir_str = tmp()
+        self.tmp_dir = pathlib.Path(tmp_dir_str)
+
+        dag = self.build_dag()
+        executor = self.run(dag)
+
+        shutil.rmtree(tmp_dir_str)
+
+        if executor.jobs_with_errors:
+            raise ValueError("Execution failed")
+
+        if len(dag.waiting_jobs) > 0 or len(dag.ready_jobs) > 0:
+            raise ValueError(
+                "The DAG has some unexecuted jobs\n"
+                f"Waiting jobs: {dag.waiting_jobs}\n"
+                f"Ready jobs: {dag.ready_jobs}\n"
+            )
+
+    @abstractmethod
+    def validate(self) -> None:
+        pass
+
+    @abstractmethod
+    def configure(self) -> None:
+        pass
+
+    @abstractmethod
+    def build_dag(self) -> DAG:
+        pass
+
+    def run(self, dag: DAG) -> BaseExecutor:
+        """Execute the DAG"""
+        self.logger.debug("Creating the scheduler")
+        resources: Dict[str, int] = {}
+        for i in range(len(self.numa_nodes)):
+            resources[f"node{i}"] = 1
+        scheduler = ThreadScheduler(
+            dag,
+            self.cores,
+            resources,
+        )
+
+        self.logger.debug("Creating the executor")
+        Executor = DryRunExecutor if self.dry_run else LocalExecutor
+        executor = Executor(scheduler)
+
+        self.logger.info("Starting execution")
+        executor.execute()
+        return executor
