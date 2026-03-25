b4cda6c8bafd079a4b7bb92a23eff66d895396ac
Don Freed <donfreed12@gmail.com>
2026-03-24 20:18:10 -0700

    Replace `colorlog` with `logging` from the standard library

--- sentieon_cli/logging.py
+++ sentieon_cli/logging.py
@@ -1,16 +1,14 @@
-import colorlog
+import logging
 
-handler = colorlog.StreamHandler()
+handler = logging.StreamHandler()
 handler.setFormatter(
-    colorlog.ColoredFormatter(
-        "%(log_color)s%(levelname)s:%(name)s:%(message)s"
-    )
+    logging.Formatter("%(levelname)s:%(name)s:%(message)s")
 )
 
 
 def get_logger(name: str):
-    """Return a logger with a colorlog handler."""
-    logger = colorlog.getLogger(name)
+    """Return a logger with a StreamHandler."""
+    logger = logging.getLogger(name)
     logger.addHandler(handler)
     logger.propagate = False
     return logger
